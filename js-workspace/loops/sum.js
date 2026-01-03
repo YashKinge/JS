4a29c05a3d674ff619012c2c0d211377b55ea260
YashKinge <yashbkinge22@gmail.com>
2026-01-03 18:16:16 +0530

    added for loop sum program

---
+++ js-workspace/loops/sum.js
@@ -0,0 +1,10 @@
+//program to display the sum of natural numbers
+
+let n = 10;
+let sum = 0;
+
+for (let i = 1; i <= n; i++) {
+  sum = sum + i;
+}
+
+console.log("Sum of numbers from 1 to " + n + " is: " + sum);
